9729b4f643ed97d01c42b760a259cb22c589a045
Derek Rein <derek.visualeffects@gmail.com>
2025-05-31 12:22:33 +0800

    Update main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -19,6 +19,24 @@ const config: StorybookConfig = {
     if (process.env.NODE_ENV === "production") {
       config.base = "/react-cie-1931/";
     }
+
+    // Fix Storybook dependency optimization issues
+    config.optimizeDeps = {
+      ...config.optimizeDeps,
+      exclude: [
+        ...(config.optimizeDeps?.exclude || []),
+        "@storybook/blocks",
+        "@storybook/components",
+        "@storybook/core-events",
+        "@storybook/docs-tools",
+        "@storybook/global",
+        "@storybook/manager-api",
+        "@storybook/preview-api",
+        "@storybook/theming",
+        "@storybook/types",
+      ],
+    };
+
     return config;
   },
 };
